65f0d9b7c7e1d247611de534621c55ba3dcd026f
Radhika Sishodiya <sishodiyaradhika@gmail.com>
2026-07-12 11:03:10 +0530

    feat: containerize PostgreSQL with Docker Compose, fix timezone and schema setup

--- src/main/java/com/radhika/payflow/PayflowApplication.java
+++ src/main/java/com/radhika/payflow/PayflowApplication.java
@@ -3,10 +3,13 @@ package com.radhika.payflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PayflowApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(PayflowApplication.class, args);
 	}
 
